2662f1d9441148baf43d39fda2f5de25fb322ffb
MIT4893-Projects <minhngoc2891211@gmail.com>
2024-05-10 11:46:04 +0700

    Fix src/ folder

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-row min-h-screen">
-      <div className="hidden sm:block w-80 bg-gray-900"></div>
-      <div className="flex-1 col-span-2"></div>
-    </div>
-  );
-}
