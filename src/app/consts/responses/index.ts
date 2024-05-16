154504d3d611afa7825e235c6e1652ea338319ff
MIT4893-Projects <minhngoc2891211@gmail.com>
2024-05-16 08:14:57 +0700

    Fix missing fields bug and blank JSON bug

--- src/app/consts/responses/index.ts
+++ src/app/consts/responses/index.ts
@@ -1,29 +1,34 @@
 import { JsonSucessResponse, JsonErrorResponse } from "@/models/response";
 import { HttpStatus } from "http-status-ts";
 
-export const ValidTokenRes = new JsonSucessResponse({
-  message: "Valid token",
-});
+export const ValidTokenRes = () =>
+  new JsonSucessResponse({
+    message: "Valid token",
+  });
 
-export const InvalidTokenRes = new JsonErrorResponse({
-  status_code: HttpStatus.UNAUTHORIZED,
-  message: "Invalid token",
-});
+export const InvalidTokenRes = () =>
+  new JsonErrorResponse({
+    status_code: HttpStatus.UNAUTHORIZED,
+    message: "Invalid token",
+  });
 
-export const MissingFieldsRes = new JsonErrorResponse({
-  status_code: HttpStatus.BAD_REQUEST,
-  message: "Required fields not found",
-});
+export const MissingFieldsRes = () =>
+  new JsonErrorResponse({
+    status_code: HttpStatus.BAD_REQUEST,
+    message: "Required fields not found",
+  });
 
-export const InvalidJsonRes = new JsonErrorResponse({
-  status_code: HttpStatus.BAD_REQUEST,
-  message: "Invalid JSON",
-});
+export const InvalidJsonRes = () =>
+  new JsonErrorResponse({
+    status_code: HttpStatus.BAD_REQUEST,
+    message: "Invalid JSON",
+  });
 
-export const LoginFailedRes = new JsonErrorResponse({
-  status_code: HttpStatus.BAD_REQUEST,
-  message: "Invalid email or password",
-});
+export const LoginFailedRes = () =>
+  new JsonErrorResponse({
+    status_code: HttpStatus.BAD_REQUEST,
+    message: "Invalid email or password",
+  });
 
 export const LoginSuccessRes = (payload: any) =>
   new JsonSucessResponse({
@@ -31,10 +36,11 @@ export const LoginSuccessRes = (payload: any) =>
     payload,
   });
 
-export const RegisterUserExistsRes = new JsonErrorResponse({
-  status_code: HttpStatus.BAD_REQUEST,
-  message: "User with this email already exists",
-});
+export const RegisterUserExistsRes = () =>
+  new JsonErrorResponse({
+    status_code: HttpStatus.BAD_REQUEST,
+    message: "User with this email already exists",
+  });
 
 export const RegisterUserSuccessRes = (payload: any) =>
   new JsonSucessResponse({
